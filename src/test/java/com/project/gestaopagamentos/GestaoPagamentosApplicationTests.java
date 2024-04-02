2c10c0fec7868f60881974dd7b1845a4cc246b58
Ingrid Monalisa <ingrid.bicudo2010@gmail.com>
2024-04-02 15:22:22 -0300

    add first configurations

---
+++ src/test/java/com/project/gestaopagamentos/GestaoPagamentosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.gestaopagamentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoPagamentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
