f56a43d5a43e4f6f0ce98915759b239f9c8ee4ab
Ingrid Monalisa <ingrid.bicudo2010@gmail.com>
2024-04-04 19:36:51 -0300

    add PagamentoResponse mapper

--- src/main/java/com/project/gestaopagamentos/mappers/PagamentoMapper.java
+++ src/main/java/com/project/gestaopagamentos/mappers/PagamentoMapper.java
@@ -1,10 +1,14 @@
 package com.project.gestaopagamentos.mappers;
 
 import com.project.gestaopagamentos.dtos.request.PagamentoRequest;
+import com.project.gestaopagamentos.dtos.response.PagamentoResponse;
 import com.project.gestaopagamentos.models.PagamentoModel;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 @RequiredArgsConstructor
@@ -13,5 +17,13 @@ public class PagamentoMapper {
     private final ModelMapper mapper;
     public PagamentoModel toPagamento(PagamentoRequest request) {
         return mapper.map(request, PagamentoModel.class);
+    }
+    public PagamentoResponse toPagamentoResponse(PagamentoModel pagamento) {
+        return mapper.map(pagamento, PagamentoResponse.class);
+    }
+    public List<PagamentoResponse> toPagamentoResponseList(List<PagamentoModel> pagamentos) {
+        return pagamentos.stream()
+                .map(this::toPagamentoResponse)
+                .collect(Collectors.toList());
     }
 }
