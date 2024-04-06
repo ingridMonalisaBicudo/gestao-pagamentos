91aa2ff621d6e596fac9d368a180950db3e470bd
Ingrid Monalisa <ingrid.bicudo2010@gmail.com>
2024-04-06 17:03:04 -0300

    Add docker-compose configuration and scripts sql

---
+++ database-docker/criar-tabela.sql
@@ -0,0 +1,25 @@
+CREATE TABLE TB_PAGAMENTOS (
+    id UUID PRIMARY KEY,
+    status VARCHAR(50) NOT NULL,
+    data_inclusao TIMESTAMP,
+    data_pagamento TIMESTAMP NOT NULL,
+    valor DECIMAL(19,2) NOT NULL,
+    descricao VARCHAR(255) NOT NULL,
+    recorrencia_id UUID NOT NULL,
+    destino_id UUID NOT NULL,
+    CONSTRAINT fk_recorrencia FOREIGN KEY (recorrencia_id) REFERENCES RecorrenciaModel(id),
+    CONSTRAINT fk_destino FOREIGN KEY (destino_id) REFERENCES DestinoModel(id)
+);
+
+CREATE TABLE TB_DESTINO (
+    id UUID PRIMARY KEY,
+    chave_pix VARCHAR(255) NOT NULL,
+    tipo_chave_pix VARCHAR(50)
+);
+
+CREATE TABLE TB_RECORRENCIA (
+    id UUID PRIMARY KEY,
+    data_final TIMESTAMP NOT NULL,
+    frequencia VARCHAR(50) NOT NULL
+);
+
