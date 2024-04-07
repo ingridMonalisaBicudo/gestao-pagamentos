735216c323560f9f4952af9e2b5fac7c8ddc969c
Ingrid Monalisa <ingrid.bicudo2010@gmail.com>
2024-04-07 13:43:13 -0300

    Remove @EntityScan @EnableJPARepositories

--- src/main/java/com/project/gestaopagamentos/GestaoPagamentosApplication.java
+++ src/main/java/com/project/gestaopagamentos/GestaoPagamentosApplication.java
@@ -6,8 +6,6 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EntityScan()
-@EnableJpaRepositories()
 public class GestaoPagamentosApplication {
 
 	public static void main(String[] args) {
