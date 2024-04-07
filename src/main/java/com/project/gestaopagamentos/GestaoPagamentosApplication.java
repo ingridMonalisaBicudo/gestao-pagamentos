272798c08641185c45eca1d341b685d2ed8429b7
Ingrid Monalisa <ingrid.bicudo2010@gmail.com>
2024-04-06 21:43:55 -0300

    Add @EntityScan and @EnableJpaPropeties in main application

--- src/main/java/com/project/gestaopagamentos/GestaoPagamentosApplication.java
+++ src/main/java/com/project/gestaopagamentos/GestaoPagamentosApplication.java
@@ -2,8 +2,12 @@ package com.project.gestaopagamentos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan()
+@EnableJpaRepositories()
 public class GestaoPagamentosApplication {
 
 	public static void main(String[] args) {
